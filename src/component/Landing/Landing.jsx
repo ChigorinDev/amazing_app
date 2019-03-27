2fbbbc8119864d345740aacea89f5b65d434dd8a
Chigorin M.L <mmchigorin@gmail.com>
2019-03-27 20:56:37 +0300

    repositry fix

---
+++ src/component/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.scss';
+
+class Landing extends React.Component { 
+    render() { 
+        return (
+            <div className="landing-wrapper">
+                <h1>Amazing landing</h1>
+            </div>
+        );
+    }
+}
